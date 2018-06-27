60a98d4c6259c494e5ba3ea1baae20f57ad7a4a5
Jorge Bau Gracia <jbaugracia@gmail.com>
2018-06-27 22:19:40 +0200

    Groovy shared library
    Example class and global variable

---
+++ src/my/organization/logic/SomeClass.groovy
@@ -0,0 +1,19 @@
+package my.organization.logic
+
+class SomeClass {
+	
+	// Properties
+	
+	private String name;
+	
+	// Methods
+	
+	public SomeClass(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		return this.name();
+	}
+} 
---
+++ vars/my/organization/vars/SomeSingleton.groovy
@@ -0,0 +1,13 @@
+/**
+ * This class behaves like a global singleton
+ */
+
+package my.organization.vars
+
+def someMethod() {
+	println "Some method!"
+}
+
+def someOtherMethod() {
+	println "Some other method!"
+}
